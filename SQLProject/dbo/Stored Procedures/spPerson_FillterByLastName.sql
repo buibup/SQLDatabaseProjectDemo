f06bd5f5a52854fd8937c4b5846e59db2f415a4f
buibup <mongkolm@hatari.net>
2018-11-20 14:29:54 +0700

    Completed with SQL Data Tools In C# - Database Creation, Management, and Deployment in Visual Studio from  IAmTimCorey

---
+++ SQLProject/dbo/Stored Procedures/spPerson_FillterByLastName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spPerson_FillterByLastName]
+	@LastName nvarchar
+AS
+
+begin
+		select [PersonId], [FirstName], [LastName]
+		from dbo.Person
+		where LastName = @LastName
+end
